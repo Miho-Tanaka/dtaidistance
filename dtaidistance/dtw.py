c4de2f0094a3868fd91a8c09760bc93d91ffe0c4
michel0v0michel <miho@to-on.com>
2019-09-20 16:01:07 +0900

    modified how to write acos

--- dtaidistance/dtw.py
+++ dtaidistance/dtw.py
@@ -141,7 +141,7 @@ def distance(s1, s2, window=None, max_dist=None,
         for j in range(j_start, j_end):
             from sklearn.metrics.pairwise import cosine_similarity
             from math import acos
-            d = min(1,max(-1,acos(cosine_similarity([s1[i]],[s2[j]])[0][0])))
+            d = acos(min(1,max(-1,cosine_similarity([s1[i]],[s2[j]])[0][0])))
             #d = (s1[i] - s2[j])**2
             if d > max_step:
                 continue
@@ -285,7 +285,7 @@ def warping_paths(s1, s2, window=None, max_dist=None,
             # print('j =', j, 'max=',min(c, c - r + i + window))
             from sklearn.metrics.pairwise import cosine_similarity
             from math import acos
-            d = min(1,max(-1,acos(cosine_similarity([s1[i]],[s2[j]])[0][0])))
+            d = acos(min(1,max(-1,cosine_similarity([s1[i]],[s2[j]])[0][0])))
             #d = (s1[i] - s2[j])**2
             if max_step is not None and d > max_step:
                 continue
